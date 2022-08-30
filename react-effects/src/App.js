3ab1212a1710016d5a86cd8dcc246a16abda75ed
junha <leejh@corezetta.com>
2022-08-30 14:16:54 +0900

    6강 useEffect

--- react-effects/src/App.js
+++ react-effects/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react"; // useState를 import시킨다
+import { useState, useEffect } from "react"; // useState와 useEffect를 import시킨다
 
 function App() {
   const [counter, setCounter] = useState(0); // 위에서 useState를 import시켜서 여기에 React.을 안써도 된다
@@ -14,6 +14,13 @@ function App() {
 
   const onClick = () => setCounter((current) => current + 1);
   console.log('render'); // counter가 변할때마다 렌더링 되어 찍힌다
+
+  const onlyOne = () => {
+    return console.log('오직 한번만 실행된다');
+  }
+  useEffect(onlyOne, []); // useEffect로 onlyOne함수가 counter가 변해도 한번만 실행된다
+  //
+
   return (
     <div>
       <h1>hello! {counter}</h1>
